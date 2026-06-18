c7d6eed4ee51ef63c4e9f378b41ca435ccf4efc1
jz237 <jrbaud@gmail.com>
2026-06-18 13:41:21 -0400

    Retire Missile Command 3JS route

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,9 +1,25 @@
 export async function onRequest(context) {
   const url = new URL(context.request.url);
+  const retiredPaths = [
+    "/games/2026-06-06/missile-command-3js",
+    "/games/2026-06-06/missile-command-3js/",
+    "/games/2026-06-06/missile-command-3js/index.html",
+  ];
 
   if (url.hostname === "www.jez237.com") {
     url.hostname = "jez237.com";
     return Response.redirect(url.toString(), 301);
+  }
+
+  if (retiredPaths.includes(url.pathname)) {
+    return new Response("Not Found\n", {
+      status: 404,
+      headers: {
+        "Cache-Control": "no-store",
+        "Content-Type": "text/plain; charset=utf-8",
+        "X-Robots-Tag": "noindex",
+      },
+    });
   }
 
   return context.next();
